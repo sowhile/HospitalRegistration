ae3e23dfb0ba29706e822e568314df656b834dfa
sowhile <wdworkmail@qq.com>
2023-02-01 00:07:17 +0800

    add delete

--- vue-admin-template-master/src/api/hospset.js
+++ vue-admin-template-master/src/api/hospset.js
@@ -1,19 +1,29 @@
 import request from '@/utils/request'
 
-//查询医院列表
-export function getHospSetList(current, limit, searchObj) {
-  return request({
-    url: `/admin/hosp/hospitalSet/findPageHospSet/${current}/${limit}`,
-    method: 'post',
-    data: searchObj //使用json传递
-  })
-}
+export default {
+  //查询医院列表
+  getHospSetList(current, limit, searchObj) {
+    return request({
+      url: `/admin/hosp/hospitalSet/findPageHospSet/${current}/${limit}`,
+      method: 'post',
+      data: searchObj //使用json传递
+    })
+  },
 
 //删除医院设置
-export function deleteHospSet(id) {
-  return request({
-    url: `/admin/hosp/hospitalSet/${id}`,
-    method: 'delete'
-  })
+  deleteHospSet(id) {
+    return request({
+      url: `/admin/hosp/hospitalSet/${id}`,
+      method: 'delete'
+    })
+  },
+  //批量删除
+  batchHospSet(idList) {
+    return request({
+      url: `/admin/hosp/hospitalSet/batchRemove`,
+      method: 'delete',
+      data: idList
+    })
+  }
 }
 
